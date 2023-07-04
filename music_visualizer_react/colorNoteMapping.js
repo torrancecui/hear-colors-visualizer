598f95eeade39febb0d03072af6d3691f7a3eb95
Torrance Cui <cuitorrance@gmail.com>
2023-07-04 00:46:18 -0700

    init

---
+++ music_visualizer_react/colorNoteMapping.js
@@ -0,0 +1,16 @@
+// "<Note>: ["<Note name>", "<Color Hex>"]
+
+export const colorNoteMapping = {
+  C: ["C4", "#C3A4A5"],
+  G: ["G4", "#DFAC9C"],
+  D: ["D4", "#EDB990"],
+  A: ["A4", "#F4D588"],
+  E: ["E4", "#DDDF8E"],
+  B: ["B4", "#BAE58C"],
+  "F#": ["C5", "#A7E498"],
+  "C#": ["G5", "#96D9BC"],
+  "G#": ["D5", "#9CCCD9"],
+  "D#": ["E5", "#ABBAD9"],
+  "A#": ["A5", "#CFC0ED"],
+  F: ["C6", "#F0CEF3"],
+};
