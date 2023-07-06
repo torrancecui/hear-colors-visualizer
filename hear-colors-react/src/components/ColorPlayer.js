a1cb66f5dfb0722181179417dfbd829ff8050291
jiyoonm <54877411+jiyoonm@users.noreply.github.com>
2023-07-05 22:17:17 -0500

    add controls and environment lighting

--- hear-colors-react/src/components/ColorPlayer.js
+++ hear-colors-react/src/components/ColorPlayer.js
@@ -3,7 +3,13 @@ import { ColorNoteMapping } from "../ColorNoteMapping";
 import { Canvas } from "@react-three/fiber";
 import * as Tone from "tone";
 import { attachEffectsChain, playSynth, stopSynth } from "./SynthUtils";
-
+import {
+  OrbitControls,
+  ContactShadows,
+  Sky,
+  Environment,
+} from "@react-three/drei";
+import Cube from "./Cube";
 export default function ColorPlayer() {
   const synth = new Tone.MonoSynth();
   attachEffectsChain(synth);
@@ -37,35 +43,51 @@ export default function ColorPlayer() {
 
       let hexColor = ColorNoteMapping[color][1];
       colorBar.push(
-        <ColorSphere
+        <Cube
           position={[x, y, 0]}
           color={hexColor}
           selected={selectedColors.includes(color)}
           onClick={() => processColorSelect(color)}
-        ></ColorSphere>
+        ></Cube>
       );
       index += 1;
     }
     return colorBar;
   }
 
-  function ColorSphere(props) {
-    const ref = useRef();
-    return (
-      <mesh {...props} ref={ref} scale={props.selected ? 1.3 : 1}>
-        <sphereGeometry args={[0.5, 32, 16]} />
-        <meshStandardMaterial color={props.color} />
-      </mesh>
-    );
-  }
+  // function ColorSphere(props) {
+  //   const ref = useRef();
+  //   return (
+  //     <mesh {...props} ref={ref} scale={props.selected ? 1.3 : 1}>
+  //       <sphereGeometry args={[0.5, 32, 16]} />
+  //       <meshStandardMaterial color={props.color} />
+  //     </mesh>
+  //   );
+  // }
 
   return (
     <div className="ColorPlayer">
       <div className="Canvas">
         <Canvas>
           <ambientLight />
+          <OrbitControls
+            enabled={true}
+            makeDefault={true}
+            enableZoom={true}
+            enablePan={true}
+          />
           <pointLight position={[10, 10, 10]} />
+          <Environment preset="city" />
+          <ContactShadows
+            frames={1}
+            position={[0, -0.5, 0]}
+            scale={10}
+            opacity={0.4}
+            far={1}
+            blur={2}
+          />
           <ColorBar></ColorBar>
+          <Sky />
         </Canvas>
       </div>
       <div className="Buttons">
